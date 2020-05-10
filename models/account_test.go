72c5ec453953cd8baff4c0e1d390702f119febe9
Vinicius Schettino <vinicius.schettino@ufjf.edu.br>
2020-05-10 16:32:09 -0300

    Base models. Test models

--- models/account_test.go
+++ models/account_test.go
@@ -7,8 +7,6 @@ import (
 	"testing"
 )
 
-
-
 func TestHashPassword(t *testing.T) {
 	hash, _ := models.HashPassword("myneatpass")
 	cost, _ := bcrypt.Cost([]byte(hash))
@@ -18,8 +16,15 @@ func TestHashPassword(t *testing.T) {
 func TestVerifyPassword(t *testing.T) {
 	password, err := models.HashPassword("myneatpass")
 	var account = models.Account{
-		Password:    password,
+		Password: password,
 	}
 	assert.NoError(t, err)
 	assert.True(t, account.VerifyPassword("myneatpass"))
+}
+
+func TestAccountToString(t *testing.T) {
+	acc := models.Account{
+		Id: 6644,
+	}
+	assert.Contains(t, acc.String(), "6644")
 }
